e23666a14a380cde9fab00dd673f60babc73b22e
Murilo Rodrigues <murilooliveira10789@gmail.com>
2025-04-14 19:39:17 -0300

    Projeto criado

---
+++ src/main/java/com/murilo/ceramicamastermongo/CeramicamastermongoApplication.java
@@ -0,0 +1,13 @@
+package com.murilo.ceramicamastermongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeramicamastermongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeramicamastermongoApplication.class, args);
+	}
+
+}
